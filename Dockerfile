8e958ff6a0662280384d968024d78bac56667f7c
zakky <kycaft1164@gmail.com>
2024-07-10 09:11:50 +0700

    update

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,12 @@
-
 FROM kong/kong-gateway:3.7.1.1
 
 # Install system dependencies
 USER root
-RUN apt-get update && apt-get install -y git unzip luarocks
-RUN luarocks install xml2lua
+RUN apt-get update && apt-get install -y git unzip luarocks zlib1g-dev
+RUN luarocks install lua-cjson
+RUN luarocks install xml2lua 1.4
+RUN luarocks install lua-zlib
+
 COPY . /usr/local/share/lua/5.1/kong/plugins/soap-transformer
 WORKDIR /usr/local/share/lua/5.1/kong/plugins/soap-transformer
 RUN luarocks make kong-plugin-soap-transformer-1.0-1.rockspec
--- soap-transformer/handler.lua
+++ soap-transformer/handler.lua
@@ -1,11 +1,84 @@
-local JsonSoapTransformer = {
-  PRIORITY = 1000,
-  VERSION = "0.0.1",
-}
+local xml2lua = require("xml2lua")
+local handler = require("xmlhandler.tree")
+local cjson = require "cjson"
+local zlib = require("zlib")
 
+function xml2json(xml)
+    local parser = xml2lua.parser(handler)
+    parser:parse(xml)
 
-function JsonSoapTransformer:access(conf)
-  kong.service.response.set_raw_body("Hello, world!")
+    a = cjson.encode(handler.root)
+    return a
+
 end
 
-return JsonSoapTransformer
+local TransformResponseHandler = {
+    PRIORITY = 1000,
+    VERSION = "1.0.0",
+}
+
+function TransformResponseHandler:header_filter(conf)
+    -- Check if Content-Type is JSON-like or XML-like
+    local content_type = ngx.header["Content-Type"]
+    if content_type then
+        if content_type:find("application/json", 1, true) or content_type:find("+json", 1, true) then
+            ngx.ctx.is_json_response = true
+            ngx.ctx.is_xml_response = false
+            ngx.header["Content-Type"] = "application/xml"  -- Change content type to XML
+            ngx.header["Accept-Encoding"] = nil
+            ngx.header["Content-Encoding"] = nil
+            kong.response.set_header("Accept-Encoding", "")
+        elseif content_type:find("application/xml", 1, true) or content_type:find("+xml", 1, true) then
+            ngx.ctx.is_json_response = false
+            ngx.ctx.is_xml_response = true
+            ngx.header["Content-Type"] = "application/json"  -- Change content type to JSON
+            ngx.header["Accept-Encoding"] = nil
+            ngx.header["Content-Encoding"] = nil
+            kong.response.set_header("Accept-Encoding", "")
+        else
+            ngx.ctx.is_json_response = false
+            ngx.ctx.is_xml_response = false
+        end
+    else
+        ngx.ctx.is_json_response = false
+        ngx.ctx.is_xml_response = false
+    end
+end
+
+function TransformResponseHandler:body_filter(conf)
+    local chunk, eof = ngx.arg[1], ngx.arg[2]
+    local ctx = ngx.ctx
+
+    if not ctx.buffer then
+        ctx.buffer = {}
+    end
+
+    if chunk ~= "" then
+        table.insert(ctx.buffer, chunk)
+        ngx.arg[1] = nil
+    end
+
+    if eof then
+        local response_body = table.concat(ctx.buffer)
+
+        if ctx.is_xml_response then
+        end
+
+        if ctx.is_json_response then
+            local jsonPay = cjson.decode(response_body)
+            if type(jsonPay) ~= "table" then
+              jsonPay = [[cjson.decode(response_body)]]
+            end
+            kong.response.set_raw_body(xml2lua.toXml(jsonPay, "Soap:Envelope"))
+        end
+        -- Clear buffer to prevent memory leaks
+        ctx.buffer = nil
+    end
+end
+
+--function TransformResponseHandler:access(conf)
+--    kong.request.set_header("Accept-Encoding", "")
+--end
+
+return TransformResponseHandler
+
